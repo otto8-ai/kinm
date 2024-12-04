3b344185962a7ce3ccfcb1330389fddd9fb57f60
Donnie Adams <donnie@acorn.io>
2024-12-04 06:51:43 -0500

    fix: stop deleting created records on compaction
    Signed-off-by: Donnie Adams <donnie@acorn.io>

--- pkg/db/statements/compact.sql
+++ pkg/db/statements/compact.sql
@@ -1,8 +1,8 @@
 WITH to_delete AS (SELECT prev.id AS id
                    FROM placeholder AS prev
                             JOIN placeholder AS cur ON (
-                       (prev.id = cur.previous_id OR
-                        (prev.id = cur.id AND (cur.deleted = 1)))
+                       ((prev.id = cur.previous_id AND prev.created IS NULL) OR
+                        (prev.id = cur.id AND cur.deleted = 1))
                            AND cur.id <= coalesce(
                                (SELECT id AS id
                                 FROM compaction
